3cb567c9f6c4dc7d838a96ddba604dd20d332b93
Wessam Saeid El Sayed <wessam.saeid33@gmail.com>
2020-01-06 12:40:19 +0200

    Update appHandler.js

--- core/appHandler.js
+++ core/appHandler.js
@@ -185,7 +185,7 @@ module.exports.userEditSubmit = function (req, res) {
 
 module.exports.redirect = function (req, res) {
 	if (req.query.url) {
-          // res.redirect(req.query.url)
+           res.redirect(req.query.url)
 	} else {
 		res.send('invalid redirect url')
 	}
